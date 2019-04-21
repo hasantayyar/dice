51a192b7d64e04b5cba608d1c29d422bb58fb772
Hasan Tayyar Besik <hasan.besik@frontiercargroup.com>
2019-04-21 20:48:42 +0200

    service worker for offloading

--- sw.js
+++ sw.js
@@ -1,9 +1,10 @@
-var APP_PREFIX = 'ApplicationName_'     // Identifier for this app (this needs to be consistent across every cache update)
-var VERSION = 'version_01'              // Version of the off-line cache (change this value everytime you want to update cache)
-var CACHE_NAME = APP_PREFIX + VERSION
-var URLS = [                            // Add URL you want to cache in this list.
-  '/{repository}/',                     // If you have separate JS/CSS files,
-  '/{repository}/index.html'            // add path to those files here
+let APP_PREFIX = 'ROLL_DICE_77875501_'     // Identifier for this app (this needs to be consistent across every cache update)
+let VERSION = 'version_01'              // Version of the off-line cache (change this value everytime you want to update cache)
+let CACHE_NAME = APP_PREFIX + VERSION
+let repo = 'dice';
+let URLS = [
+  `/${repo}/`,
+  `/${repo}/index.html`
 ]
 
 // Respond with cached resources
